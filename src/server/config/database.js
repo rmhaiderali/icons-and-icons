4f893d147159a81f8fd498379169c0adb7029ade
Haider Ali <rmha64@gmail.com>
2024-01-02 21:25:52 +0500

    format files

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const { log } = console
+const { log } = console;
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/iconsandicons"
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/iconsandicons";
 
-mongoose.connection.on("error", (err) => console.error(err))
+mongoose.connection.on("error", (err) => console.error(err));
 
-mongoose.connection.once("open", () => log("connected to the mongodb"))
+mongoose.connection.once("open", () => log("connected to the mongodb"));
 
-await mongoose.connect(MONGODB_URI)
+await mongoose.connect(MONGODB_URI);
